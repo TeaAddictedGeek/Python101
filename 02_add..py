e4d3394013e66fca2411698484bb6ed9035c0fc6
Andy <andrea.berman@gmail.com>
2025-06-24 15:16:10 -0400

    initial commit of basic Python functions

---
+++ 02_add..py
@@ -0,0 +1,5 @@
+a = int(input("Enter first number: "))
+# a = int(a)
+b = int(input("Enter second number: "))
+# b = int(b)
+print(a + b)
---
+++ 05_string_formatting.py
@@ -0,0 +1,19 @@
+template = "{} is getting a grade of {} in Python"
+a = "John"
+a1 = "B"
+b = "Jane"
+b1 = "B+"
+c = "Andy"
+c1 = "A+"
+
+s1 = template.format(a, a1)
+
+s2 = template.format(b, b1)
+
+s3 = template.format(c, c1)
+
+print(s1)
+print(s2)
+print(s3)
+
+print(f"{c} is gearing for a grade of {c1} in Python")
